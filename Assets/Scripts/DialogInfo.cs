981d8285b349456b52d21af014e1f5328ab17de8
my-laptop <2655667253@qq.com>
2024-05-04 01:24:40 +0800

    剥离出存储对话信息的类

---
+++ Assets/Scripts/DialogInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 存储对话信息
+/// </summary>
+public class DialogInfo  {
+    public UiManager.NpcNames Name;
+    public string Content;
+}
+
