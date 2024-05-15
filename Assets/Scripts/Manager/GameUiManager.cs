153ee9e5bff971332e4991e276b367e22e8197eb
my-laptop <2655667253@qq.com>
2024-05-15 17:20:15 +0800

    完善:战斗场景中的怪物血条

--- Assets/Scripts/Manager/GameUiManager.cs
+++ Assets/Scripts/Manager/GameUiManager.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Linq.Expressions;
 using System.Security.Cryptography;
 using System.Threading;
 using DG.Tweening;
@@ -25,10 +26,11 @@ public partial class GameUiManager : MonoBehaviour {
 
     public GameObject MainSceneMonsters;
 
+    [SerializeField] private float PosOffset = 50f;         // 血条位置的偏移值
     [SerializeField] private GameObject BattleSceneMonster; // 战斗场景中的怪兽
-    [SerializeField] private SliderManager MonsterSlider;   // 怪兽血条
+    [SerializeField] private SliderManager MonsterSlider;   // 怪兽血条,
     public Vector3 InitBattleMonsterGlobPos;  // 战斗场景中的怪兽初始位置
-    public Vector3 InitMonsterSliderGlobPos;  // 怪兽血条的初始位置
+    public Vector3 InitMonsterSliderGlobPos;  // 怪兽血条的初始位置 (注意,它默认就是屏幕坐标,无需转换)
 
     /// <summary>
     /// 游戏层级mask
@@ -47,15 +49,34 @@ public partial class GameUiManager : MonoBehaviour {
         Instance = this;
         originalSize = hpMaskImage.rectTransform.rect.width;
 
-        InitBattleMonsterGlobPos = Camera.main.WorldToScreenPoint(BattleSceneMonster.transform.position);
-        InitMonsterSliderGlobPos = MonsterSlider.transform.position;
+        InitMonsterBarPos(PosOffset);
         MonsterSlider.mainSlider.minValue = GameManager.Instance.MonsterMinHp;
         MonsterSlider.mainSlider.maxValue = GameManager.Instance.MonsterMaxHp;
         MonsterSlider.mainSlider.value = GameManager.Instance.MonsterCurrentHp;
     }
 
     private void Update() {
-        SetBar(GameManager.Instance.LunaCurrentHp / GameManager.Instance.LunaMaxHp, GameManager.Instance.LunaCurrentMp / GameManager.Instance.LunaMaxMp);
+        UpdateLunaBar(GameManager.Instance.LunaCurrentHp / GameManager.Instance.LunaMaxHp, GameManager.Instance.LunaCurrentMp / GameManager.Instance.LunaMaxMp);
+        UpdateMonsterBar();
+    }
+
+    /// <summary>
+    /// 每次进入战斗场景都需要记录一下怪兽的初始位置(因为实际上它是跟着摄像头移动的,每次的初始位置都不一样)
+    /// </summary>
+    /// <param name="offset">血条偏移</param>
+    private void InitMonsterBarPos(float offset = 0) {
+        InitBattleMonsterGlobPos = Camera.main.WorldToScreenPoint(BattleSceneMonster.transform.position);
+        InitMonsterSliderGlobPos = InitBattleMonsterGlobPos;
+        InitMonsterSliderGlobPos.x += offset;
+    }
+
+    /// <summary>
+    /// 更新怪兽血条的位置,跟随怪兽移动
+    /// </summary>
+    private void UpdateMonsterBar() {
+        MonsterSlider.mainSlider.value = GameManager.Instance.MonsterCurrentHp;
+        float distance = Camera.main.WorldToScreenPoint(BattleSceneMonster.transform.position).x - InitBattleMonsterGlobPos.x;
+        MonsterSlider.transform.DOMoveX(InitMonsterSliderGlobPos.x + distance, 0);
     }
 
     /// <summary>
@@ -63,10 +84,14 @@ public partial class GameUiManager : MonoBehaviour {
     /// </summary>
     /// <param name="hpFillPercent">血条填充百分比</param>
     /// <param name="MpFillPercent">蓝条填充百分比</param>
-    public void SetBar(float hpFillPercent, float MpFillPercent) {
+    public void UpdateLunaBar(float hpFillPercent, float MpFillPercent) {
         // 通过RectTransform的锚点来设置图片的宽度,第一个参数我们用锚点的水平轴向,第二个size是填充的百分比
         hpMaskImage.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, hpFillPercent * originalSize);
         mpMaskImage.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, MpFillPercent * originalSize);
+    }
+
+    private void ShowMonsterHpSlider(bool isShow = true) {
+        MonsterSlider.gameObject.SetActive(isShow);
     }
 
     public void ShowBattleUi(bool enter = true) {
@@ -84,13 +109,18 @@ public partial class GameUiManager : MonoBehaviour {
             StartCoroutine(PerformMonsterLogic());
             // 播放正常音乐
             AudioManager.Instance.PlayMusic(AudioManager.Instance.NormalClip);
+
+            // 开启战斗场景时
         } else {
+            // TODO 怪物没有死的时候,重新进入战斗场景的血量应该保持
             GameManager.Instance.MonsterCurrentHp = GameManager.Instance.MonsterMaxHp;
             GameManager.Instance.CanControlLuna = false;
             // 播放战斗音乐
             AudioManager.Instance.PlayMusic(AudioManager.Instance.BattleClip);
         }
         BattleBackGroundPanel.SetActive(enter);
+        InitMonsterBarPos(PosOffset);
+        ShowMonsterHpSlider(enter);
     }
 
     /// <summary>
